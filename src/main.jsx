0611613e652c745d43a417bb2a7ce91b8a38f265
PRNLGBR <99623826+PRNLGBR@users.noreply.github.com>
2026-02-03 07:11:54 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./App.css"; // opcional, só se quiser estilos globais
+import "./App.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
